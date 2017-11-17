9b398844c68b48a4b4b317009b8184686d7d451f
kohei0408 <kohei.hamada@yahoo.com>
2017-11-17 13:37:41 +0900

    rename2

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :repo_url, "git@github.com:koheihamada/stock.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :linked_files, %w{ config/secrets.yml }
-set :linked_files, %w{ config/setting.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
