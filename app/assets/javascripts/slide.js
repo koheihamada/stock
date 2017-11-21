2c4d5d392fa1cbd272b1af3e18db600cbc83d756
kohei0408 <kohei.hamada@yahoo.com>
2017-11-21 13:21:20 +0900

    js

---
+++ app/assets/javascripts/slide.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.single-item').slick({
+    accessibility: true,
+    adaptiveHeight: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    fade: true,
+  });
+});
