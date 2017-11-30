27dfba0ea116bc18a40421c339cfcc4c1ab53087
kohei0408 <kohei.hamada@yahoo.com>
2017-11-30 20:08:56 +0900

    js modal payment

---
+++ app/assets/javascripts/payment.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('#open').on('click', function() {
+    $('#overlay, #modalWindow').fadeIn();
+  });
+
+  $('#close, #overlay').on('click', function() {
+    $('#overlay, #modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('#modalWindow').outerWidth();
+    let ch = $('#modalWindow').outerHeight();
+
+    $('#modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
