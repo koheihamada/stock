67dd5b1f710a5bbd1d30c7c5e9ec5e55bc16516e
kohei0408 <kohei.hamada@yahoo.com>
2017-12-03 11:38:42 +0900

    css and js

---
+++ app/assets/javascripts/selling.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('#pay').on('click', function() {
+    $('#overlay, #modalWindow').fadeIn();
+  });
+
+  $('#end, #close').on('click', function() {
+    $('#overlay, #modalWindow').fadeOut();
+  });
+
+  locateCenter();
+  $(window).resize(locateCenter);
+
+  function locateCenter() {
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('#modalWindow').outerWidth();
+    let ch = $('#modalWindow').outerHeight();
+
+    $('#modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h - ch) / 2) + 'px'
+    });
+  }
+});
