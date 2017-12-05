87c132e00fbb4fe237d30b275351862b1dd4c09e
kohei0408 <kohei.hamada@yahoo.com>
2017-12-05 18:20:49 +0900

    bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-
+  #
   # config.after_initialize do
   #   Bullet.enable  = true   # bullet を有効にする
   #
